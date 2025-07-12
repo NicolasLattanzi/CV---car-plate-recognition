b9fb9b834daf28653bf0dd48261b66a9d8a76d3e
edobughi <168537499+edobughi@users.noreply.github.com>
2025-07-12 17:03:49 +0200

    Update data.py
    rimosso MyImageFolder e il custom dataset

--- data.py
+++ data.py
@@ -52,29 +52,6 @@ class CarPlateDataset(Dataset):
 def train_test_split(dataset, train=0.7, test=0.3):
     return torch.utils.data.random_split(dataset, [train, test])
 
-class MyImageFolder(ImageFolder):
-
-    def __getitem__(self, index: int) -> tuple[any, any]:
-        path, target = self.samples[index]
-        sample = self.loader(path)
-        if self.transform is not None:
-            sample = self.transform(sample)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return sample, path
-    
-
-def create_dataset(path):
-
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                            std=[0.229, 0.224, 0.225])
-    ])
-
-    return MyImageFolder(root=path, transform=transform)
 
 
 CHARS = ['京', '沪', '津', '渝', '冀', '晋', '蒙', '辽', '吉', '黑',
