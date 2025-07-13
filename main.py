d0f94b2f4ff1af6116c7a8d09f8887cd4e090b6f
edobughi <168537499+edobughi@users.noreply.github.com>
2025-07-13 11:13:16 +0200

    Update main.py
    creata una mini pipeline che sceglie una immagine random dal dataset di test, la passa nel primo modello, la trasforma, la passa nel secondo modello e stampa la stringa ottenuta

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import torch
-
+import random
 from network import LPRNet
 from network import create_model_Detection
+from train import test_dataset
+import utils
 
 #carico modello detection 
 modelDet = torch.load("modelDetection.pth", map_location="cpu")
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+modelDet = modelDet.to(device)
 
+#picking random image
+test_size = len(test_dataset)
+int=random.randint(0,test_size-1)
+image, label=test_dataset[int]
+
+#compute detection on random image
+output1=modelDet(image) #output1=vertici della targa nell'immagine originale
+
+input2=utils.LP_photo(image, output1) #immagine 94x24 ottenuta dall'immagine originale
 
 #carico modello riconoscimento e i suoi pesi
 
@@ -16,4 +29,11 @@ modelReco=LPRNet(class_num=num_classes, dropout_rate=dropout_rate)
 
 state_dict=torch.load("Final_LPRNet_model.pth", map_location="cpu")
 modelReco.load_state_dict(state_dict)
+
+
+#passo il tensore ottenuto al modello
+output2=modelReco(input2) #stringa con i caratteri della targa
+print(output2)
+
+
 
