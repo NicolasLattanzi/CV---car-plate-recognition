8a45175f2805778dde569066f1e0f2c883e29fea
NicolasLattanzi <lattanzi.1992502@studenti.uniroma1.it>
2025-07-17 23:09:36 +0200

    new model trained

--- train.py
+++ train.py
@@ -7,18 +7,18 @@ import data
 ###### hyper parameters ########
 
 batch_size = 32
-num_epochs = 1
+num_epochs = 2
 learning_rate = 0.001
 
 ###############################
 
-dataset = data.CarPlateDataset("../CCPD2019")
+dataset = data.CarPlateDataset("..\CCPD2019")
 train_dataset, test_dataset = data.train_test_split(dataset)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
-train_size = len(train_loader)/8
-test_size = len(test_loader)/8
+train_size = len(train_loader)
+test_size = len(test_loader)
 
 modelDet = network.build_resnet()
 # checking if gpu is available, otherwise cpu is used
@@ -54,7 +54,6 @@ for epoch in range(num_epochs):
         # printing error every X batch
         if (i + 1) % 50 == 0:
             print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{train_size}], Loss: {loss.item():.4f}")
-        if i >= train_size: break
 
     avg_train_loss = train_loss / train_size
     print(f"Epoch [{epoch+1}/{num_epochs}] training completed. Average Loss: {avg_train_loss:.4f}")
@@ -73,7 +72,6 @@ for epoch in range(num_epochs):
 
             if (i + 1) % 50 == 0:
                 print(f"Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{train_size}], Loss: {loss.item():.4f}")
-            if i >= test_size: break
 
     avg_test_loss = test_loss / test_size
     print(f"Epoch [{epoch+1}/{num_epochs}] test completed. Average Loss: {avg_test_loss:.4f}\n")
