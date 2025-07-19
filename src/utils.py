3531e821c74eca52782387da177483bf787225b6
edobughi <168537499+edobughi@users.noreply.github.com>
2025-07-19 20:49:11 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,16 +2,8 @@ import torch
 from torchvision import transforms
 from torchvision.transforms import functional
 import torch.nn.functional as f
-
-CHARS = ['京', '沪', '津', '渝', '冀', '晋', '蒙', '辽', '吉', '黑',
-         '苏', '浙', '皖', '闽', '赣', '鲁', '豫', '鄂', '湘', '粤',
-         '桂', '琼', '川', '贵', '云', '藏', '陕', '甘', '青', '宁',
-         '新',
-         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
-         'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-         'W', 'X', 'Y', 'Z', 'I', 'O', '-'
-         ]
+import cv2
+import numpy as np
 
 provinces = ["皖", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑", "苏", 
              "浙", "京", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤", "桂", "琼", "川", "贵", "云", "藏", 
@@ -35,6 +27,7 @@ def plate_from_image_path(path: str):
     plate_chars = plate.split('_')
     # ''.join(plate_chars)
     return list(map(int, plate_chars))
+
 
 
 def crop_photo(image, vertices):
@@ -67,52 +60,6 @@ def crop_photo(image, vertices):
     transform = transforms.Resize((24, 94))
     return transform(cropped_img)
 
-def tensorToString(output):
-    probs = f.softmax(output, dim=2) #estraggo le probabilita dal tensore che sia un carattere specifico
-    pred_indices = probs.argmax(dim=2) #seleziono per ogni indice quella piu probabile
-    #pred_indices = pred_indices.cpu().numpy().tolist()  # trasforma in lista di liste di int
-    risultati=[]
-    #chiamo il CTC decoder per ogni lista di int, ogni lista mi da una stringa, che metto in risultati
-    for sequence in pred_indices:
-        decoded = ctc_greedy_decode(sequence, ...)
-        risultati.append(decoded)
-        
-    risultatoUnico=''.join(risultati) #creo una stringa unica dalle varie componenti ottenute nel for.
-    return risultatoUnico
-
-
-#def tensorToString(output):
-    print(output)
-    prebs = prebs.cpu().detach().numpy()
-    preb_labels = list()
-    for i in range(prebs.shape[0]):
-            preb = prebs[i, :, :]
-            preb_label = list()
-            for j in range(preb.shape[1]):
-                preb_label.append(np.argmax(preb[:, j], axis=0))
-            no_repeat_blank_label = list()
-            pre_c = preb_label[0]
-            if pre_c != len(CHARS) - 1:
-                no_repeat_blank_label.append(pre_c)
-            for c in preb_label: # dropout repeate label and blank label
-                if (pre_c == c) or (c == len(CHARS) - 1):
-                    if c == len(CHARS) - 1:
-                        pre_c = c
-                    continue
-                no_repeat_blank_label.append(c)
-                pre_c = c
-            preb_labels.append(no_repeat_blank_label)
-    return preb_labels
-
-
-def ctc_greedy_decode(pred_indices, blank=len(CHARS)-1, char_list=CHARS):
-    decoded = []
-    prev = None
-    for idx in pred_indices:
-        if idx != blank and idx != prev:
-            decoded.append(char_list[idx])  # <-- qui ottieni il carattere
-        prev = idx
-    return ''.join(decoded)
 
 def lpDecoder(license,pr_list=provinces, char_list=ads):
     
